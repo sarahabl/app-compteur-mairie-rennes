6f36fb2988123bb8e9b7a6cb8faef85dcc64a683
sarahabl <sarahablonet.studio@gmail.com>
2024-06-26 11:26:42 +0200

    mise en place du compteur

---
+++ src/components/Counter.jsx
@@ -0,0 +1,35 @@
+// src/Counter.jsx
+import React, { useState } from 'react';
+
+const Counter = () => {
+  // Déclaration d'une nouvelle variable d'état, que nous appellerons "count".
+  const [count, setCount] = useState(0);
+
+  // Fonction pour incrémenter le compteur
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  // Fonction pour décrémenter le compteur
+  const decrementCount = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  // Fonction pour remettre le compteur à zéro
+  const resetCount = () => {
+    setCount(0);
+  };
+
+  return (
+    <div>
+      <h1>Compteur: {count}</h1>
+      <button onClick={incrementCount}>Incrémenter</button>
+      <button onClick={decrementCount}>Décrémenter</button>
+      <button onClick={resetCount}>Remettre à zéro</button>
+    </div>
+  );
+};
+
+export default Counter;
